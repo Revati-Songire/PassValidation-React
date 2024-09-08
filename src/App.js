0b503ef44f6c756b954fac0083792ea1a1324ed5
Revati <revatisongire@gmail.com>
2024-09-08 18:31:05 +0530

    passvalid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,121 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { FaArrowCircleLeft } from "react-icons/fa";
+import { FaArrowCircleRight } from "react-icons/fa";
 
 function App() {
+  const [password, setPassword] = useState('');
+  const [inputColor, setInputColor] = useState(''); 
+  const [leftItems, setLeftItems] = useState([]);
+  const [rightItems, setRightItems] = useState([]);
+
+  
+  const validatePassword = () => {
+    let letters = 0;
+    let symbols = 0;
+    let digits = 0;
+
+    for (let i = 0; i < password.length; i++) {
+      const char = password[i];
+      if ((char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')) {
+        letters++;
+      } else if (char >= '0' && char <= '9') {
+        digits++;
+      } else {
+        symbols++;
+      }
+    }
+
+    const isValid = letters >= 5 && symbols >= 1 && digits >= 3;
+
+    if (isValid) {
+      setInputColor('green'); 
+      alert("Password is Valid!")
+      const newItem = { id: Date.now(), name: password, checked: false };
+      setLeftItems([...leftItems, newItem]);
+      console.log(leftItems)
+      setPassword('');
+    } else {
+      setInputColor('red'); 
+      alert("Password must contain min 5 letters,1 symbol and 3 digits")
+    }
+  };
+
+  const clickCheckBoxLeft = (checked,index) => {
+    console.log(checked," " ,index)
+    let arr=[...leftItems]
+    console.log(arr[index],"before")
+    arr[index].checked=checked
+    console.log(arr[index],"after")
+    setLeftItems(arr)
+  };
+
+  const clickCheckBoxRight = (checked,index) => {
+    console.log(checked," " ,index)
+    let arr=[...rightItems]
+    console.log(arr[index],"before")
+    arr[index].checked=checked
+    console.log(arr[index],"after")
+    setRightItems(arr)
+  };
+
+  const movetoRight = () => {
+    let arr=[]
+    let arr2=leftItems.filter((ele)=>ele.checked== false)
+    arr=leftItems.filter((ele)=>{
+      if(ele.checked==true){
+        ele.checked=false
+        return true
+    }
+      return false;})
+    setRightItems([...arr,...rightItems])
+    setLeftItems(arr2)
+  };
+
+  const movetoLeft = () => {
+    let arr=[]
+    let arr2=rightItems.filter((ele)=>ele.checked== false)
+    arr=rightItems.filter((ele)=>{
+      if(ele.checked==true){
+        ele.checked=false
+        return true
+    }
+      return false;})
+    setLeftItems([...arr,...leftItems])
+    setRightItems(arr2)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <input type="text" value={password} onChange={(e) => {setPassword(e.target.value); setInputColor(''); }}
+        placeholder="Enter Password" style={{ backgroundColor: inputColor }} />
+
+      <button onClick={validatePassword} className='submitbtn'>SUBMIT</button>
+
+      <div className="card">
+        <div className="left-div">
+          {leftItems.map((item,index) => (
+            <div key={item.id}>
+              <input type="checkbox" checked={item.checked} onChange={(e) => clickCheckBoxLeft(e.target.checked,index)}/>
+              <span>{item.name}</span>
+            </div>
+          ))}
+        </div>
+
+         <div className="middle-div">
+           <button className='left-arrow' onClick={movetoLeft}> <FaArrowCircleLeft /> </button>
+           <button className='right-arrow' onClick={movetoRight}> <FaArrowCircleRight /> </button>
+         </div>
+        
+         <div className="right-div">
+         {rightItems.map((item,index) => (
+            <div key={item.id+"right"}>
+              <input type="checkbox" checked={item.checked} onChange={(e) => clickCheckBoxRight(e.target.checked,index)}/>
+              <span>{item.name}</span>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
